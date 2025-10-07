717ebc93a3f87823735e4cbde98567d9c00ce800
Vitor de Freitas Ribeiro <programador.vitor.ribeiro@hotmail.com>
2025-10-07 18:56:40 -0300

    altered display logic

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,23 +1,19 @@
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
-  const married = isMarried;
 
-  const partnerLabel =
-    sex === 'm'
-      ? 'wife'
-      : 'husband';
+  let partnerLabel = 'I am not married';
+
+  if (isMarried && sex === 'm') {
+    partnerLabel = `${partnerName} is my wife`;
+  } else if (isMarried && sex === 'f') {
+    partnerLabel = `${partnerName} is my husband`;
+  }
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age != null && <p className="Person__age">I am {age}</p>}
-      {married ? (
-        <p className="Person__partner">
-          {partnerName} is my {partnerLabel}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <p className="Person__partner">{partnerLabel}</p>
     </section>
   );
 };
